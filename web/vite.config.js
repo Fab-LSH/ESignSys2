676a48286538b8e6a3edd0090e5966bd8e10253e
Fab-LSH <13230785+Fab-LSH@user.noreply.gitee.com>
2025-08-27 16:49:02 +0800

    827

--- web/vite.config.js
+++ web/vite.config.js
@@ -14,10 +14,20 @@ export default defineConfig({
   server:{
     host: '0.0.0.0',
     port: 5678, // 前端端口
+    allowedHosts: [
+      '2f2fa3c1.r6.cpolar.cn',
+      'localhost',
+      '127.0.0.1',
+      '172.29.80.64'
+    ],
     proxy:{
       '/api': {
-        target: 'http://localhost:5050', // 后端实际端口
+        // target: 'http://localhost:5050', // 后端实际端口
+        target: 'http://47.93.189.200:5050', // 直接使用公网IP
         changeOrigin: true,
+        // 添加安全配置
+        secure: false,
+        ws: true,
       },
     },
   },
